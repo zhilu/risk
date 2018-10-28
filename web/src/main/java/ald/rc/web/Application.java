2a99343682849b67bd316983ec532d58b07ad631
shi <shi@192.168.0.103>
2018-10-28 20:19:49 +0800

    page

--- web/src/main/java/ald/rc/web/Application.java
+++ web/src/main/java/ald/rc/web/Application.java
@@ -1,11 +1,14 @@
 package ald.rc.web;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
 }
---
+++ web/src/main/java/ald/rc/web/controller/LoginController.java
@@ -0,0 +1,15 @@
+package ald.rc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("index")
+	public String login() {
+		System.out.println("hehehe");
+		return "login";
+	}
+
+}
